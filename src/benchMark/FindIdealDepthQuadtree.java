dc1d7b1ba4476da7ccb55f932db22a4941de673f
Logan <phamphuocsang5850@gmail.com>
2024-05-05 16:23:49 +0700

    add sep for number in benchmark folder to see the number easily

--- src/benchMark/FindIdealDepthQuadtree.java
+++ src/benchMark/FindIdealDepthQuadtree.java
@@ -1,17 +1,16 @@
 package benchMark;
 
 import java.util.Random;
-
 import maps.Map2D;
 import utils.Rectangle;
 
 public class FindIdealDepthQuadtree {
     public static void main(String[] args) {
-        int[] numberOfNodes = new int[]{1000000,10000000,20000000,50000000,70000000,100000000};
+        int[] numberOfNodes = new int[]{1000000, 10000000, 20000000, 50000000, 70000000, 100000000};
 
         for (int numberOfNode : numberOfNodes) {
             System.out.println("##########################################################################");
-            System.out.println("Number of nodes: " + numberOfNode);
+            System.out.println("Number of nodes: " + String.format("%,d", numberOfNode));
             for (int i = 1; i < 7; i++) {
                 Runtime runtime = Runtime.getRuntime();
                 Rectangle boundary = new Rectangle(10000000 / 2, 10000000 / 2, 10000000, 10000000);
@@ -21,7 +20,7 @@ public class FindIdealDepthQuadtree {
                 qt.generateRandomData(numberOfNode);
                 long endTime = System.currentTimeMillis();
                 System.out.println("------------------------------------");
-                System.out.println("Depth: " + i + ", Capacity per node: " + Map2D.calculateIdealCapacity(numberOfNode, i) + ", Time: " + (endTime - startTime) + "s, Memory: " + (runtime.totalMemory() - runtime.freeMemory()) / 1000000 + "MB");
+                System.out.println("Depth: " + i + "; Capacity per node: " + String.format("%,d", Map2D.calculateIdealCapacity(numberOfNode, i)) + "; Time: " + (endTime - startTime) + "ms; Memory: " + String.format("%,d", (runtime.totalMemory() - runtime.freeMemory()) / 1000000) + "MB");
                 // Benchmark search time
                 benchmarkSearchTime(qt);
             }
@@ -42,13 +41,13 @@ public class FindIdealDepthQuadtree {
             Rectangle searchRect = new Rectangle(x, y, width, height);
 
             long searchStart = System.currentTimeMillis();
-            qt.search(searchRect, null, null, 50); // Perform the search
+            qt.search(searchRect, null, null, 1000); // Perform the search
             long searchEnd = System.currentTimeMillis();
 
             totalSearchTime += (searchEnd - searchStart);
         }
 
         double averageSearchTime = totalSearchTime / (double) numberOfSearches;
-        System.out.println("Average search time for " + numberOfSearches + " searches: " + averageSearchTime + "ms");
+        System.out.println("Average search time for " + numberOfSearches + " searches: " + String.format("%.2f", averageSearchTime) + "ms");
     }
 }
